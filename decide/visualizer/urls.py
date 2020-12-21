7ea44c36824d481f1b7a7ed8fda1a04ce46568d9
marti <marti@LAPTOP-AEP4C04M>
2020-12-21 18:19:28 +0100

    Language switch functionality implemented

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import VisualizerView
-
+from django.conf.urls import url
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
+    url('i18n/', include('django.conf.urls.i18n')),
 ]
+
