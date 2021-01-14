7d09c2092d0d9afff3c7ca7e0a5e9936f175baa8
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-01-14 02:16:00 +0100

    Bot inactivo y nuevo token

--- decide/botDis.py
+++ decide/botDis.py
@@ -1,19 +1,32 @@
-import discord
+from discord.ext import commands
 import os
 
 dis = 'Nzk4ODg0NDIyODUzNTkxMDQx.X_7hGA'
-token = '.E04N6tkXDMNNjahAF6jwH-8KgCU'
+token = '.lYiyxyHWmpedGIdmmKHL-xnfq50'
 TOKEN =  dis+token
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
-#@client.command()
-#async def enviarMensaje():
-#   canal = client.channels.cache.find()
+@client.command(name='exit', help='Cierra el bot')
+async def salir(ctx):
+    await ctx.send('Me voy, ¡adios!')
+    await client.logout()
+    await client.close()
+
+@client.command(name='votaciones', help='Devuelve todas las votaciones con sus resultados')
+async def votaciones(ctx):
+    
+
+    response = 'Hola, estoy funcionando.'
+    await ctx.send(response)
+
+#async def enviarMensaje(mensaje):
+#   canal = client.get_channel(799052293080743946)
+#  message.canal.send(mensaje)
 
 
 client.run(TOKEN)
