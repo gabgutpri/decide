abe32b618076f4025908c8860dbe4b666d081c98
angcabdom <angcabdom@alum.us.es>
2021-01-14 00:24:19 +0000

    <style> Eliminados importaciones no utilizadas
    Se han eliminado varias importaciones para selenium que no se estan utilizando
    
    <tarea: #121>

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -23,11 +23,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import time
 
 class VotingTestCase(BaseTestCase):
 
