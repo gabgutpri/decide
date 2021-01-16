b0056c7b6c7f2d3b794eb7fc480c5b08e3ed96a9
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-01-16 20:42:45 +0100

    Correcion a testgraficadonut

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -414,7 +414,7 @@ class TestGraficaDonut():
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
         self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) > .pmd-floating-action-btn > img").click()
-        assert self.driver.find_element(By.CSS_SELECTOR, "th:nth-child(1) > .heading").text == "Donut Chart"
+        assert self.driver.find_element(By.CSS_SELECTOR, "th:first-child(1) > .heading").text == "Donut Chart"
         #Sacamos una captura de pantalla de la gráfica de barras para comprobar que esta existe, dentro de la imagen screenshotgraficadonut.png aparece esta gráfica.
         #Podemos comprobar que existe la gráfica y que tiene el número correcto de opciones.
         self.driver.find_element_by_id('myChart').screenshot('screenshotgraficadonut.png')
