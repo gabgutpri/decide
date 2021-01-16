01b85744ecd7406d7c429af349499e3d8e98a3dc
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-01-16 04:26:56 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -141,6 +141,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.conf.global_settings',
             ],
         },
     },
