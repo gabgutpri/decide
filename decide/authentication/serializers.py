e69da0aa3fdd6447c7ed789511529935831a435b
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-01-16 01:44:59 +0100

    Update serializers.py

--- decide/authentication/serializers.py
+++ decide/authentication/serializers.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff')
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
