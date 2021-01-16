f71a6b856ee743e0b1c3fdfac79a738e1d756c5a
Thiloparn <48439369+Thiloparn@users.noreply.github.com>
2021-01-16 11:38:28 +0100

    Anadido test de traduccion al frances en el Contact Us

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -58,6 +58,26 @@ class TestTraduccion():
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) img").click()
     assert self.driver.find_element(By.ID, "text").text == "Résultats"
 
+class TestTraduccionContactUs():
+  def setup(self):
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("--no-sandbox")
+    self.driver = webdriver.Chrome(options=options)
+  
+  def teardown(self):
+    self.driver.quit()
+  
+  def test_traduccionContactUs(self):
+    self.driver.get("https://picaro-decide.herokuapp.com/visualizer/5/")
+    self.driver.set_window_size(1552, 840)
+    element = self.driver.find_element(By.CSS_SELECTOR, ".fa-language")
+    actions = ActionChains(self.driver)
+    actions.move_to_element(element).perform()
+    self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) img").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".btn-secondary:nth-child(2)").click()
+    assert self.driver.find_element(By.CSS_SELECTOR, ".contactForm > h2").text == "Envoyer en méssage"
+
 class TestQuestion():
   def setup(self):
     options = webdriver.ChromeOptions()
@@ -95,6 +115,7 @@ class TestContactUs():
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.headless = True
+        options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(options=options)
         self.driver.fullscreen_window()
   
@@ -311,6 +332,7 @@ class TestGraficaBarras():
         
     def tearDown(self):
         self.driver.quit()
+
 
 # if __name__ == '__main__':
 #     unittest.main()
