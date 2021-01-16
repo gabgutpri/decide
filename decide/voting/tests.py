13482572cb7386b261b1e7edc25f50c6bbbda7cd
gonroho <49684111+gonroho@users.noreply.github.com>
2021-01-16 21:48:23 +0000

    <test> Test upgraded
    Se completa la votación creada con el nuevo parametro
    
    Creación de Tests de Votaciones con URL #22

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -224,3 +224,26 @@ class VotingTestCase(BaseTestCase):
         v.auths.add(a)
 
         return v
+    
+    def test_complete_voting_withurl(self):
+        v = self.create_voting_withurl()
+        self.create_voters(v)
+
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+        clear = self.store_votes(v)
+
+        self.login()  # set token
+        v.tally_votes(self.token)
+
+        tally = v.tally
+        tally.sort()
+        tally = {k: len(list(x)) for k, x in itertools.groupby(tally)}
+
+        for q in v.question.options.all():
+            self.assertEqual(tally.get(q.number, 0), clear.get(q.number, 0))
+
+        for q in v.postproc:
+            self.assertEqual(tally.get(q["number"], 0), q["votes"])
