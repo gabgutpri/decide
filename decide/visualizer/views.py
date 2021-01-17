04191f2f2f14547feeb63dd2578f8992cad855e1
davsotpon <davsotpon@alum.us.es>
2021-01-17 18:06:51 +0100

    Arreglo de la vista Visualizer

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
 from voting.models import Voting
-
+from django.conf import settings
 
 from base import mods
 
@@ -14,54 +14,41 @@ class VisualizerView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-
+        
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
             if r[0]['start_date'] is None:
-                print('asd')
+                print('Votación no comenzada')
             elif r[0]['end_date'] is None:
-
+                
                 context['options'] = json.dumps(r[0]['question']['options']) # Datos para gráfica en tiempo real. (gabgutpri, visualización)
-                votosOpcion = votos_opcion(vid, r[0])
-                context['votosOpcion'] = votosOpcion                         # Fin de datos para gráfica
+                opciones = r[0]['question']['options']
+                numOp=[]
+                
+                for i in range(len(opciones)):
+                    numOp.append(opciones[i]['number'])
+                
+                votos = mods.get('store',params={'voting_id':vid})
+                print('buenas a todos')
+                votosPorOpcion = []
+                for op in numOp:
+                    cuenta = 0
+                    for v in votos:
+                        if(op==v['b']):
+                            cuenta = cuenta + 1
+                    votosPorOpcion.append(cuenta)
+                
+                context['votosOpcion'] = votosPorOpcion                         # Fin de datos para gráfica
 
-                numero_votos = get_numero_votos(vid)
+                
+                numero_votos= len(votos)
                 context['numero_votos'] = numero_votos
+                
         except:
             raise Http404
 
         return context
-
-    # Método para obtener los votos por opción en una votación
-    def votos_opcion(vid, voting):
-        opciones = voting['question']['options']
-        numOp=[]
-        for i in range(len(opciones)):
-            numOp.append(opciones[i]['number'])
-        votos = mods.get('store',params={'voting_id':vid})
-        votosPorOpcion = []
-        for op in numOp:
-            cuenta = 0
-            for v in votos:
-                if(op==v['b']):
-                    cuenta = cuenta + 1
-            votosPorOpcion.append(cuenta)
-        return votosPorOpcion
-
-
-    def get_numero_votos (vid):
-        
-        
-        #census = mods.get('admin', entry_point= '/census/census', params={'voting_id': vid})
-        #print('asd')
-        numero_votos=0
-        voters = mods.get('store',params={'voting_id':vid})
-        voters_id = [v['voting_id'] for v in voters]
-        numero_votos= len(voters_id)
-
-        return numero_votos
-
 
 
 class ContactUs(TemplateView):
@@ -80,7 +67,7 @@ class VisualizerHome(TemplateView):
     template_name = 'visualizer/visualizer_home.html'
     # Método para obtener los votos de todas las votaciones (gabgutpri, visualización)
 
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         queryset = Voting.objects.all()
@@ -91,6 +78,7 @@ class VisualizerHome(TemplateView):
                 cuenta = [v['voting_id'] for v in votos]
                 listaVotos.append(len(cuenta))
             return listaVotos
+
         # Parte de la gráfica --- gabgutpri (visualizacion)
         votaciones = mods.get('voting', params={}) # Todas las votaciones
         context['votaciones']= json.dumps(votaciones) # Transformación para que no de problemas en el script JS
@@ -100,6 +88,8 @@ class VisualizerHome(TemplateView):
 
         context.update({'votings': queryset})
         return context
+    
+    
 
     
     
